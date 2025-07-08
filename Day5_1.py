e83140351d2af94aae40a3455ba966f0914e4408
sayult <2948174604@qq.com>
2025-07-08 12:37:32 +0800

    小小的修改

--- Day5_1.py
+++ Day5_1.py
@@ -2,9 +2,9 @@ file = open('diary.txt', 'w', encoding='utf-8')
 
 file.write('2025年7月7日 星期一\n')
 file.write('今天学习文件操作！\n')
-file.write('今天是美好的一天！')
+file.write('今天是美好的一天！\n')
 
 file.write('2025年7月8日 星期二\n')
-file.write('今天学会了pr的视频与音频分离！')
+file.write('今天学会了pr的视频与音频分离！\n')
 
 file.close()
